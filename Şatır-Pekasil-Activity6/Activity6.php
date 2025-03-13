bc5218abf54fbcfd42c6817fcdeae86ba01ade1a
TalhaSatir <155726361+TalhaSatir@users.noreply.github.com>
2025-03-13 15:18:28 +0300

    Update Activity6.php

--- Şatır-Pekasil-Activity6/Activity6.php
+++ Şatır-Pekasil-Activity6/Activity6.php
@@ -3,41 +3,6 @@
 <head>
   <meta charset="UTF-8" />
   <title>Activity6 - Registration Form</title>
-  <style>
-    /* Part 3 tip: make label & input inline-block to control width/height */
-    .form-group {
-      display: inline-block;
-      width: 200px;
-      margin: 5px 0;
-      vertical-align: top; 
-    }
-    label {
-      display: inline-block;
-      width: 80px;
-      font-weight: bold;
-    }
-    input[type="text"], 
-    input[type="password"],
-    input[type="email"],
-    select {
-      width: 100px;
-      padding: 3px;
-      margin-left: 5px;
-    }
-    textarea {
-      width: 200px;
-      height: 60px;
-      margin-left: 5px;
-    }
-    .radio-group,
-    .checkbox-group {
-      width: auto;
-      display: inline-block;
-    }
-    .submit-button {
-      margin-top: 10px;
-    }
-  </style>
 </head>
 <body>
   <h1>Registration Form</h1>
@@ -75,7 +40,6 @@
         <option value="USA">USA</option>
         <option value="Germany">Germany</option>
         <option value="France">France</option>
-        <!-- Add more as needed -->
       </select>
     </div>
     <br/>
@@ -99,6 +63,19 @@
         <label for="male" style="font-weight:normal;">Male</label>
         <input type="radio" name="sex" value="Female" id="female" />
         <label for="female" style="font-weight:normal;">Female</label>
+      </div>
+    </div>
+    <br/>
+    
+    <div class="form-group">
+      <label>Language:</label>
+      <div class="checkbox-group">
+        <input type="checkbox" name="languages[]" value="English" id="english" />
+        <label for="english" style="font-weight:normal;">English</label>
+        <input type="checkbox" name="languages[]" value="French" id="french" />
+        <label for="french" style="font-weight:normal;">French</label>
+        <input type="checkbox" name="languages[]" value="German" id="german" />
+        <label for="german" style="font-weight:normal;">German</label>
       </div>
     </div>
     <br/>
